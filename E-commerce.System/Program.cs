4b2c51ce4fa67376f5cec23738fd01aaeeb85775
123eslam <eslameltamalawy124@gmail.com>
2025-07-05 18:53:01 +0300

    Add project files.

---
+++ E-commerce.System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_commerce.System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
